1faa052afbb372f4c756513350c1f653d20a51a4
Ben Heaton <benheaton@live.co.uk>
2025-09-23 17:39:29 +0100

    Second commit

--- jqGame.js
+++ jqGame.js
@@ -1,32 +1,39 @@
-$(document).ready(() => {
+$(document).ready(function () {
     const $all_game_cards = $('.game_card');
     let card_count = 0;
+    let current_flipped_cards = [];
+    let card_1 = current_flipped_cards[0]
+    let card_2 = current_flipped_cards[1]
 
     // On load. Ready to play.
     $all_game_cards.addClass('game_card_bs');
 
     // On click, flip card.
-    function flip_card() {
-        $all_game_cards.on('click', function(event) {
-            $(event.currentTarget).removeClass('game_card_bs').addClass('game_card_fs');
-            card_count += 1;
-            current_cards($(event.currentTarget));
-            check_card_count();
-        }
-    }
+    $all_game_cards.on('click', function (event) {
+        $(event.currentTarget).removeClass('game_card_bs').addClass('game_card_fs');
 
-    function current_cards(card_1, card_2) {
-        let card_1 = ""
-    }
+        // Add 1 to the card counter when a card is flipped over.
+        card_count+= 1;
 
-    function check_card_count() {
+        // Also add the card ID to the tracker array.
+        track_cards(event.currentTarget.id)
+
+        // Checks if two cards are flipped over.
         if (card_count === 2) {
-            check_card_match();
+            check_cards()
         }
+    });
+
+    // Tracks current IDs of flipped over cards.
+    function track_cards(card) {
+        current_flipped_cards.push(card);
     }
 
-    function check_card_match(card_1, card_2) {
+    // If two cards are flipped over, compare them.
+    function check_cards(card_1, card_2) {
+        if (card_1 === card_2) {
 
+        }
     }
 
 }); // End of .ready
